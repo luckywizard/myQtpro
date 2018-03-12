afe1f52793e0789436ee5090d068e2b71b39e15b
LG <lg_leegang@163.com>
2018-03-12 16:01:37 +0800

    add cout output
    Signed-off-by: LG <lg_leegang@163.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <QCoreApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    using namespace std;
+
+    cout << "hello world!" << endl;
 
     return a.exec();
 }
